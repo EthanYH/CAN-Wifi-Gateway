82b588aa58dd21a211f9d30b52c169e8459c322a
EthanYH <yonghan007@hotmail.com>
2016-08-25 19:59:41 +0900

    clean comments

--- ESP8266/BlynkBlink_StringOut/BlynkBlink_StringOut.ino
+++ ESP8266/BlynkBlink_StringOut/BlynkBlink_StringOut.ino
@@ -28,7 +28,6 @@
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-//#include <SimpleTimer.h>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
@@ -40,10 +39,6 @@ char ssid[] = "ssid";
 char pass[] = "pass";
 
 char serialinput[10];
-//float t = float(random(100, 300)) / 10;
-// Format: 1 decimal place, add ℃
-//String str = String(t, 1) + "℃";
-//String strout = String(serialinput[]);
 
 WidgetTerminal terminal(V4);
 
@@ -65,19 +60,6 @@ void setup()
 // the same Virtual Pin as your Terminal Widget
 BLYNK_WRITE(V4)
 {
-
-  // if you type "Marco" into Terminal Widget - it will respond: "Polo:"
-  /*if (String("Marco") == param.asStr()) {
-    terminal.println("You said: 'Marco'") ;
-    terminal.println("I said: 'Polo'") ;
-  } else {
-
-    // Send it back
-    terminal.print("You said:");
-    terminal.write(param.getBuffer(), param.getLength());
-    terminal.println();
-  }*/
-  
   // Ensure everything is sent
   terminal.flush();
 }
@@ -87,18 +69,9 @@ void loop()
 {
   Blynk.run();
   
-  //Blynk.virtualWrite(V0,"ID  Length");
-  //Blynk.virtualWrite(V1,"D0  D1  D2  D3  D4  D5  D6  D7");
-  //t = float(random(100, 300)) / 10;
-  //str = String(t, 1) + "℃";
-  
   if (Serial.available() > 0) 
   {
-      //Serial.readBytes(serialinput[0],32);
-      //strout=Serial.readString();
-      
       terminal.write(Serial.read());
-      //terminal.println();
   }
 }
 
